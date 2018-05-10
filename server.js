ac39b1cba94b82f716f2ff88ec6c594f224c1f72
Amol Dumbre <amol.dumbre@hp.com>
2018-05-10 19:35:23 +0530

    Angular page refresh issue: Updated server.js file & forward all non route to index.html file

--- server.js
+++ server.js
@@ -43,10 +43,6 @@ const express = require('express'),
     var distDir = __dirname + "/dist";
     app.use(express.static(distDir));    
     
-    app.get('/*', function(req,res) {
-        res.sendFile(path.join(__dirname+'/dist/index.html'));
-    });
-
     const port = process.env.PORT || 4000;
     const server = app.listen(port,function(){
     console.log('Listening on port ' + port);
@@ -65,4 +61,8 @@ const express = require('express'),
         var err = new Error('Route Not Found');
         err.status = 404;
         next(err);
+    });    
+
+    app.get('/*', function(req,res) {
+        res.sendFile(path.join(__dirname+'/dist/index.html'));
     });    
